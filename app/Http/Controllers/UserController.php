7a885a0fedcb80e4a23caecb4a9c7f2b5b47e47d
Amanda Vanika <amandavanika4@gmail.com>
2024-04-21 18:04:20 +0700

    praktikum 4 p7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,13 +24,20 @@ class UserController extends Controller
     
         $activeMenu = 'user'; // set menu yang sedang aktif
     
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
-    }
+        $level = LevelModel::all();
+    
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'level' => $level]);
+    }    
 
     // Ambil data user dalam betuk json untuk datatables
     public function list(Request $request) 
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
+
+        // Filter data user berdasarkan level_id
+            if ($request->level_id) {
+                $users->where('level_id', $request->level_id);
+            }
 
         return DataTables::of($users)
             ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)

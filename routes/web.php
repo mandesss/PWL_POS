cce0e67600d632a38a93c0697e51b6ef0606cff6
Amanda Vanika <amandavanika4@gmail.com>
2024-04-25 14:55:31 +0700

    praktikum p9

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\ManagerController;
 use App\Http\Controllers\POSController;
 use App\Http\Controllers\StokController;
 use App\Http\Controllers\PenjualanController;
@@ -109,4 +112,25 @@ Route::group(['prefix' => 'penjualan'], function() {
     Route::get('/create', [PenjualanController::class, 'create']);   // menampilkan halaman form tambah user
     Route::post('/', [PenjualanController::class, 'store']);         // menyimpan data user baru
     Route::get('/{id}', [PenjualanController::class, 'show']);       // menampilkan detail user
+});
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('proses_register', [AuthController::class, 'proses_register'])->name('proses_register');
+
+//kita atur juga untuk middleware menggunakan group pada routing
+//didalamnya terdapat group untuk mengecek kondisi login
+//jika user yang login merupakan admin maka akan diarahkan ke AdminController
+//jika user yang login merupakan manager maka akan diarahkan ke ManagerController
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::group(['middleware' => ['cek_login:1']], function () {
+        Route::resource('admin', AdminController::class);
+    });
+    Route::group(['middleware' => ['cek_login:2']], function () {
+        Route::resource('manager', ManagerController::class);
+    });
 });

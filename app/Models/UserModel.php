2b988e31aa263f069092b837928d3f6b9dc7b469
Amanda Vanika <amandavanika4@gmail.com>
2024-05-25 11:09:47 +0700

    P10 praktikum 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,24 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class UserModel extends Authenticatable implements JWTSubject
-
 {
-    public function getJWTIdentifier(){
-        return $this->getKey();
-    } 
-    public function getJWTCustomClaims(){
-        return [];
-    } 
-
     use HasFactory;
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id','username','nama','password'];
-    public function level():BelongsTo
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    protected $table = 'm_user'; //mendefinisikan nama tabel yg digunakan oleh model
+    public $timestamps = true;
+    protected $primaryKey = 'user_id'; //mendefinisikan primary key dr tabel yg digunakan
+    protected $fillable = ['user_id', 'level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }

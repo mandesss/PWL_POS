e31b1ff23b9b8d9aff5b20ebdda5fb32e9d31e52
Amanda Vanika <amandavanika4@gmail.com>
2024-06-25 17:39:53 +0700

    P12 Praktikum E

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -10,32 +10,20 @@ class FileUploadController extends Controller
     {
         return view('file-upload');
     }
+
+
     public function prosesFileUpload(Request $request)
     {
-        //dump($request->berkas);
-        //dump($request->file('file'));
-        //return "Pemrosesan file upload di sini";
         $request->validate([
             'berkas' => 'required|file|image|max:500',]);
             $extfile = $request->berkas->getClientOriginalName();
             $namaFile = 'web-' . time() . "." . $extfile;
-            $path = $request->berkas->storeAs('uploads', $namaFile);
+            $path = $request->berkas->storeAs('public', $namaFile);
+
+            $pathBaru = asset('storage/' . $namaFile);
             echo "Proses upload berhasil, data disimpan pada: $path";
+            echo "<br>";
+            echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
             //echo $request->berkas->getClientOriginalName() . "lolos validasi";
-        /*if ($request->hasFile('berkas')) {
-            echo "path(): " . $request->berkas->path();
-            echo "<br>";
-            echo "extension(): " . $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): " . $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): " . $request->berkas->getSize();
-        } else {
-            echo "Tidak ada berkas yang diupload";
-        }*/
     }
 }
